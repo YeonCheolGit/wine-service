08dcb0fefdc5acbb80af6254ba3244af0805e889
yeon <yeoncheol.jang@gmail.com>
2021-11-07 03:26:18 +0900

    initial commit

---
+++ src/main/java/com/example/wineservice/WineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.wineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineServiceApplication.class, args);
+    }
+
+}
